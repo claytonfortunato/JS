9861a8d352d1e655506ea2aa0760c57cf7a828e5
claytonfortunato <claytonreis10@gmail.com>
2022-08-18 20:26:40 -0300

    js

---
+++ Aula042/main.js
@@ -0,0 +1,22 @@
+/* OBJETOS
+Objetos são basicamente variáveis com muitos valores dentro.
+
+Ex const carro = {marca:"ford", modelo:"ka", ano: 2015}
+
+Os valores dentro de um objeto são chamados propriedades.
+
+Objetos também podem ter métodos. Um Método é uma função colocadas dentro de uma propriedade.
+*/
+
+const carro = {
+    marca: "ford",
+    modelo: "ka",
+    ano: 2015,
+    placa: "ABC-1234",
+    buzina: function () { alert('Biiii') },
+    completo: function () {
+        return "A marca é " + this.marca + " e o modelo é: " + this.modelo
+    }
+};
+
+console.log(carro.completo());
---
+++ Aula043/main.js
@@ -0,0 +1,23 @@
+/* EVENTOS
+Eventos são ações disparadas pela interação dos usuários na página.
+É o correto manejo desses eventos que tornam as páginas interativas e dinâmicas.
+
+Existem muitos eventos.Veja os mais utilizados:
+
+onclick -> Disparado quando recebe um click.
+ondblclick -> Disparado quando clique duplo.
+onmouseover -> Disparado quando o mouse está sobre.
+onmouseout -> Disparado quando o mouse é movido para fora do elemento.
+onmousemove -> Disparado quando o mouse é movido no elemento.
+onmousedown -> Disparado quando o clique do botão foi pressionado.
+onmouseup -> Disparado quando o clique do botão é liberado.
+onfocus -> Disparado quando o elemento recebe o foco. Válido oara input
+onchange -> Disparado quando existe uma mudança no conteudo. "Ao mudar".
+onblur -> Disparado quando o elemento perde o foco.
+onkeydown -> Disparado quando uma tecla é pressionada.
+onkeypress -> Disparado quando uma tecla é pressionada e solta.
+onkeyup -> Disparado qunado uma tecla é solta sobre um elemento.
+onload -> Disparado quando a página terminou de ser carregada.Body.
+onresize -> Disparado quando há um redimencionamento da janela.
+
+ */
