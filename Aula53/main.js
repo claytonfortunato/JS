5e1a95abfc33c6df733e32f4305bf224e0d57ac7
claytonfortunato <claytonreis10@gmail.com>
2022-10-04 19:15:36 -0300

    Aula funçao

---
+++ Aula53/main.js
@@ -0,0 +1,19 @@
+// Criar um aplicativo de frases motivacionais
+
+// declaration - declaração de função
+// function sstatement
+function createPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é mãe do aprendizado')
+}
+
+// executar a função
+// rodar, chamar, invocar
+// execute, run, call, invoke
+createPhrases()
+createPhrases()
+createPhrases()
+createPhrases()
+
+console.log('fim do programa ')
