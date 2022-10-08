709849ece20e8e64b3c3b326a527d114f9c267b8
claytonfortunato <claytonreis10@gmail.com>
2022-10-08 19:40:27 -0300

    Aula JS

--- Aula057/main07.js
+++ Aula057/main07.js
@@ -2,4 +2,7 @@
 
 // Separe um texto que contem espaços, em um novo array onde cada texte é uma poisoção de array.Depois disso, transforme o array em um texto e onde eram espaços, coloque _
 
-
+let phrase = "Eu quero viver o Amor!"
+let myArray = phrase.split(" ")
+let phraseWithUnderscore = myArray.join("_")
+console.log(phraseWithUnderscore)
