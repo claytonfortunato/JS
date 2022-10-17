e1acf33717c55a7ef0bc093cc780d7267b29e1a2
claytonfortunato <claytonreis10@gmail.com>
2022-10-16 23:05:46 -0300

    Aula JS

---
+++ Aula063/main02.js
@@ -0,0 +1,11 @@
+/** Sistema de gastos famíliar
+
+    crie um objeto possuirá 2 propiedades, ambas do tipo
+    array:
+        * receitas : []
+        * despesas:  []
+    
+    Agora, crie uma função que irá calcular o total de receitas
+    e despesas e irá mostrar uma menssagem se a família está com
+    salddo positivo ou negativo, seguido do vaalor do saldo
+ */
