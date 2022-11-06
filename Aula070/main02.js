b911a1ad96f7486d816f4f105a43913b955d0819
claytonfortunato <claytonreis10@gmail.com>
2022-11-05 23:08:43 -0300

    Aula JS

---
+++ Aula070/main02.js
@@ -0,0 +1,17 @@
+// Asysnc / Await com Fetch
+
+// fetch('https://api.github.com/users/maykbrito)
+// .then( response => response.json())
+// .then( data => fetch(data.repos_url))
+// .then( res => res.json())
+// .then( d => console.log(d))
+// .catch( err => console.log(err))
+
+async function start() {
+    const url = "https://api.github.com/users/maykbrito";
+    const user = await fetch(url).then( r => r.json())
+    const userRepos = await fetch(user.repos_url).then(r => r.json());
+    console.log(userRepos);
+}
+
+start().catch(e => console.log(e))
