ea0be853d8f18759636e640611af8f52ba7aec2f
claytonfortunato <claytonreis10@gmail.com>
2022-11-09 23:26:50 -0300

    JS

--- Aula072/main.js
+++ Aula072/main.js
@@ -8,7 +8,7 @@ function getUsers() {
 }
 
 function getUser() {
-    fetch(`${url}/1`)
+    fetch(`${url}/18`)
       .then(response => response.json())
       .then(data => {
         userName.textContent = data.name
@@ -18,5 +18,26 @@ function getUser() {
       .catch(error => console.error(error))
 }
 
+function addUser(newUser) {
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(newUser),
+    headers: {
+      "Content-type": "application/json;
+      charset=UFT-8"
+    }
+  })
+  .then(response => response.json())
+  .then(data => alertApi.textContent = data)
+  .catch(error => console.error(error))
+}
+
+const newUser = {
+  name: "Olivia Zars",
+  avatar: "http://lorempixel.com/400/200",
+  city: "Rio do sul"
+}
+
+addUser(newUser)
 getUsers()
 getUser()
