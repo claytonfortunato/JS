b5a0b8da89f03624642402070155a8628c602c5f
claytonfortunato <104373308+claytonfortunato@users.noreply.github.com>
2022-11-21 23:52:08 -0300

    jogo da velha

---
+++ Jogodavelha/main.js
@@ -0,0 +1,6 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#statusText");
+const resetBtn = document.querySelector("resetBtn");
+const winConditions = [
+
+];
