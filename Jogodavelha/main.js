c757060b0cdde1394df093d0b204de76244bedc4
claytonfortunato <104373308+claytonfortunato@users.noreply.github.com>
2022-11-24 23:46:05 -0300

    jogo

--- Jogodavelha/main.js
+++ Jogodavelha/main.js
@@ -1,6 +1,15 @@
-const cells = document.querySelectorAll(".cell");
-const statusText = document.querySelector("#statusText");
-const resetBtn = document.querySelector("resetBtn");
-const winConditions = [
+const cellElements = document.querySelectorAll("[data-cell]");
 
-];
+for (const cell of cellElements) {
+    cell.addEventListener("click", handleClick, { once: true });
+}
+
+const handleClick = () => {
+    // Colocar a marca (X ou Circulo)
+    const cell = e;
+    // Verificar por vitória
+
+    // Verificar por empate
+
+    // Mudar símbolo
+}
