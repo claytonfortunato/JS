028bd69ff88cb4fa5ae8f621ff9f1b6a3f824cc2
claytonfortunato <104373308+claytonfortunato@users.noreply.github.com>
2022-11-26 00:05:52 -0300

    Aula

--- Jogodavelha/main.js
+++ Jogodavelha/main.js
@@ -1,15 +1,50 @@
 const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.querySelector("[data-board]");
+
+let isCircleTurn;
+
+const startGame = () => {
+    for (const cell of cellElements) {
+        cell.addEventListener("click", handleClick, { once: true });
+    }
+
+    isCircleTurn = false;
+
+    board.classList.add("x");
+};
+
+const placeMark = (cell, classToAdd) => {
+    cell.classList.add(classToAdd);
+};
+
+const swapTurns = () => {
+    isCircleTurn = !isCircleTurn;
+
+    board.classList.remove("circle");
+    board.classList.remove("x");
+
+    if(isCircleTurn) {
+        board.classList.add("circle");
+    } else {
+        board.classList.add("x");
+    }
+};
+
+const handleClick = (e) => {
+    // Colocar a marca (X ou Circulo)
+    const cell = e.target;
+    const classToAdd = isCircleTurn ? "circle" : "x";
+
+    placeMark(cell, classToAdd);
+
+    // Verificar por vitória
+    // Verificar por empate
+    // Mudar símbolo
+    swapTurns();
+};
 
 for (const cell of cellElements) {
     cell.addEventListener("click", handleClick, { once: true });
 }
 
-const handleClick = () => {
-    // Colocar a marca (X ou Circulo)
-    const cell = e;
-    // Verificar por vitória
-
-    // Verificar por empate
-
-    // Mudar símbolo
-}
+startGame();
