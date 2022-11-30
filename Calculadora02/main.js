e6a4705e6e3bf97af62dd2cddd9aed45e1e765d6
claytonfortunato <104373308+claytonfortunato@users.noreply.github.com>
2022-11-29 21:12:44 -0300

    Calculadora

---
+++ Calculadora02/main.js
@@ -0,0 +1,10 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationsButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const previousOperand = document.querySelector("[data-previous-operand]");
+const currentOperand = document.querySelector("[data-current-operand]");
+
+class Calculator {
+    
+}
