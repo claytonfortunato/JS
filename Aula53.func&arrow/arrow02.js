595cfd655d18ecffd31a43399ea6494a9c5595a2
claytonfortunato <104373308+claytonfortunato@users.noreply.github.com>
2022-12-18 19:16:25 -0300

    Aulas

---
+++ Aula53.func&arrow/arrow02.js
@@ -0,0 +1,12 @@
+// Arrow functions
+
+const names = [
+    'Gabriel',
+    'João',
+    'José',
+    'Michael Jackson'
+];
+
+const resNames = names.map( names => names.length);
+
+console.log(resNames)
